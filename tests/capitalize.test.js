0eb6344d581d87d14be82c1a77087f3dab1113ec
zebpaa <skvorcov_90@mail.ru>
2024-05-25 16:20:52 +0400

    fixed test capitalize

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,8 @@
-import { capitalize } from '../src/capitalize.js';
+import { strict as assert } from "node:assert";
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+import { capitalize } from "../src/capitalize.js";
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(""), "");
+assert.equal(capitalize("hello"), "Hello");
 
-console.log('Все тесты пройдены!');
+console.log("Все тесты пройдены!");
